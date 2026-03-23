e311d345b7fd0e5ff9dabffcedb53d65bb7c75cb
Lazarević Milan <lazarevic.milan07@gmail.com>
2026-03-23 18:00:26 +0100

    new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.workoutlog"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
