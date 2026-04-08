8d6deaa872d22ed687eff1657e2a78326535dd43
Lazarević Milan <lazarevic.milan07@gmail.com>
2026-04-08 10:49:41 +0200

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.workoutlog"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.1.4"
+        versionCode = 8
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
